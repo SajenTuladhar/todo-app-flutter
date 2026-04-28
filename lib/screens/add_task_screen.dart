31ff77e5703faa6f1e765d1425da229fc6768601
Sajen Tuladhar <119946976+SajenTuladhar@users.noreply.github.com>
2026-04-28 16:35:24 +0545

    Implement checkbox state management with parent-child callback logic

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,14 +6,21 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(30),
+      padding: EdgeInsets.only(
+        top: 20,
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+        left: 32,
+        right: 32,
+      ),
       child: SizedBox(
         height: 250,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(
               'Add Task',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 26,
                 color: Colors.grey.shade800,
@@ -21,17 +28,18 @@ class AddTaskScreen extends StatelessWidget {
               ),
             ),
             TextField(
-              autofocus: false,
-              decoration: InputDecoration(border: OutlineInputBorder()),
+              autofocus: true,
+              decoration: InputDecoration(border: OutlineInputBorder(),),
             ),
             MaterialButton(
-              
               padding: EdgeInsets.all(12),
               color: Colors.grey.shade900,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadiusGeometry.circular(12),
               ),
-              onPressed: () {},
+              onPressed: () {
+
+              },
               child: Text('Add', style: TextStyle(color: Colors.white)),
             ),
           ],
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,16 +1,51 @@
- import 'package:flutter/material.dart';
- 
- 
- class TaskTile extends StatelessWidget {
-  const TaskTile({
-    super.key,
-  });
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatefulWidget {
+  const TaskTile({super.key});
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is the first task'),
-      trailing: Checkbox(value: false, onChanged: (value) => true,),
+      title: Text(
+        'This is the first task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckbox: (bool? value) {
+          setState(() {
+            isChecked = value!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({
+    required this.checkBoxState,
+    required this.toggleCheckbox,
+    super.key,
+  });
+  final bool checkBoxState;
+  final ValueChanged<bool?> toggleCheckbox;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.grey.shade900,
+      value: checkBoxState,
+      onChanged: toggleCheckbox,
     );
   }
 }
